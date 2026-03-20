37206f9f1da351b96a9f98f9c0d5c7aaeeb37ce7
CAJALEWI <cjlew1234@gmail.com>
2026-03-20 13:14:49 -0700

    Added Contact Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TitlePage from './pages/TitlePage';
 import About from './pages/About';
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TitlePage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import Navigation from '../components/Navigation';
+import '../Main.css';
+
+const Contact = () => {
+    return(
+        <>
+          <Navigation />
+          <main className = "contact-page">
+            
+          </main>
+        </>
+    );
+};
+
+export default Contact;
