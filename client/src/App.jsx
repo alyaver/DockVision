27ea0999ddf43dbe628e597ece6c3c56e7595999
BuildTools <unconfigured@null.spigotmc.org>
2026-03-22 00:28:17 -0700

    Implemented Confirmation Page *Boilerplate* for users starting test runs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TitlePage from './pages/TitlePage';
 import About from './pages/About';
 import Contact from './pages/Contact'
+import Dashboard from './pages/Dashboard';
+import Confirmation from './pages/Confirmation';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<TitlePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
     </Router>
   );
