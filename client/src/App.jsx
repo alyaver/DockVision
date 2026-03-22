679c8ab33e6c92580518bc46c4a34aa9ad87f02c
dmanganti <dannmanganti@gmail.com>
2026-03-22 15:00:29 -0700

    completed dummy test page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TitlePage from './pages/TitlePage';
 import About from './pages/About';
-import Contact from './pages/Contact'
+import Contact from './pages/Contact';
+import TestPage from './pages/running-test/TestPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<TitlePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/running-test" element={<TestPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/running-test/TestPage.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState} from "react";
+import Navigation from "../../components/Navigation";
+import { useNavigate } from "react-router-dom";
+
+function TestPage() {
+
+    const navigate = useNavigate();
+
+    // dummy logs
+    const testLogs = [
+        'Container started',
+        'Launching Service...',
+        'Connected to DB'
+    ];
+    
+    const [logs, setLogs] = useState([]);
+
+    // adds logs to page
+    const rollingLog = () => {
+        let i = 0;
+
+        const interval = setInterval(() => {
+            if (i < testLogs.length) {
+
+                // display next log
+                setLogs((prev) => [...prev, testLogs[i]]);
+
+                i++;
+            }
+            else { 
+                clearInterval(interval);
+            }
+        }, 1000);
+
+        return interval;
+    };
+
+    useEffect(() => {
+        const interval = rollingLog();
+
+        return () => clearInterval(interval)
+        
+    }, []);
+
+    return (
+        <>
+            <Navigation />
+            <div style={{ padding: '20px'}}>
+                <h1>Running Test</h1>
+
+                <button onClick={() => navigate('/dashboard')}>
+                    Return to Dashboard
+                </button>
+
+                <p>Run ID: 12345</p>
+                <p>Elapsed Time: 12:34</p>
+                <p>ETA: 56:78</p>
+
+                <h3>Running Tests</h3>
+                <ul>
+                    <li>Test1: Passed</li>
+                    <li>Warning</li>
+                    <li>Running</li>
+                    <li>Waiting...</li>
+                </ul>
+
+                <h3>Live Log Panel</h3>
+                <div style={{ border: '1px solid black', padding: '10px', minHeight: '120px' }}>
+                    {logs.map((log, index) => (
+                        <p key={index}>{log}</p>
+                    ))}
+                </div>
+
+                <h3>Latest Screenshot</h3>
+                <div style={{ border: '1px solid black', height: '150px', width: '250px' }}>
+                    (placeholder)
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default TestPage;
+
