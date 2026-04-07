f242a397e66d51421d64b8e388a7c7207dfe28bd
CAJALEWI <cjlew1234@gmail.com>
2026-04-06 18:11:38 -0700

    Password Requirement added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,18 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
 import Navigation from '../components/Navigation';
 import '../SignIn.css';
 import '../NavBar.css';
 
 const SignIn = () => {
+  const [password, setPassword] = useState("");
+
+  const rules = [
+    { label: "At least 12 characters", valid: password.length >= 12 },
+    { label: "Contains a number", valid: /\d/.test(password) },
+    { label: "Has an uppercase letter", valid: /[A-Z]/.test(password) },
+  ];
+
   return(
     <>
       <Navigation />
@@ -13,8 +22,10 @@ const SignIn = () => {
             <div className="sign-in-info-fields">
               <h2 className="value-text">Email</h2>
               <input className="sign-in-value-box" type="text" placeholder="Email" />
+
               <h2 className="value-text">Password</h2>
-              <input className="sign-in-value-box" type="text" placeholder="Password" />
+              <input className="sign-in-value-box" type="password" value ={password} 
+              onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
               <button className="sign-in-button">
                 Sign In
               </button>
