cf255f8df522d7e545a7e8149a80159aaf6d3d6f
CAJALEWI <cjlew1234@gmail.com>
2026-04-06 17:02:32 -0700

    Sign In frontend done

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 import Navigation from '../components/Navigation';
 import '../SignIn.css';
 import '../NavBar.css';
@@ -6,8 +7,21 @@ const SignIn = () => {
   return(
     <>
       <Navigation />
-        <main className = "sign-in-page">
-
+        <main className="sign-in-page">
+          <section className="sign-in-hero">
+            <h1 className="sign-in-heading">Sign In</h1>
+            <div className="sign-in-info-fields">
+              <h2 className="value-text">Email</h2>
+              <input className="sign-in-value-box" type="text" placeholder="Email" />
+              <h2 className="value-text">Password</h2>
+              <input className="sign-in-value-box" type="text" placeholder="Password" />
+              <button className="sign-in-button">
+                Sign In
+              </button>
+              <Link to="/forgot-password" className="sign-in-secondary-link">Forgot Password</Link>
+              <Link to="/registration" className="sign-in-secondary-link">Create Account</Link>
+            </div>
+          </section>
         </main>
     </>
   );
