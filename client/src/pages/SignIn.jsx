acd986944baccb61961d0cb2bf3341ef07087a6b
CAJALEWI <cjlew1234@gmail.com>
2026-04-12 14:51:04 -0700

    Added Email Rules for Sign In Page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,12 +6,37 @@ import '../NavBar.css';
 
 const SignIn = () => {
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-  const rules = [
+  const validExtensions = [".com", ".org", ".net", ".gov", ".edu", ".mil"];
+  
+  const emailParts = email.split("@");
+  const localPart = emailParts.length === 2 ? emailParts[0] : "";
+  const domainPart = emailParts.length === 2 ? emailParts[1] : "";
+
+  const passRules = [
     { label: "At least 12 characters", valid: password.length >= 12 },
     { label: "Contains a number", valid: /\d/.test(password) },
     { label: "Has an uppercase letter", valid: /[A-Z]/.test(password) },
   ];
+
+  const emailRules = [
+    { label: "Contains exactly one @ symbol", valid: emailParts.length === 2 },
+    { label: "Starts with local-part", valid: localPart.length > 0 },
+    { label: "Ends with domain-part", valid: domainPart.length > 0 },
+    { label: "Local-part is at most 64 characters", valid: localPart.length > 0 && localPart < 65 },
+    { label: "Local-part does not start or end with .",
+      valid:
+        localPart.length > 0 &&
+        !localPart.startsWith(".") &&
+        !localPart.endsWith(".")
+    },
+    { label: "Domain-part is at most 255 characters", valid: domainPart.length > 0 && domainPart < 256 },
+    { label: "Domain-part must end with a valid extension", valid: validExtensions.some((ext) => domainPart.endsWith(ext)) }
+  ];
+
+  const isPasswordValid = passRules.every((rule) => rule.valid);
+  const isEmailValid = emailRules.every((rule) => rule.valid);
 
   return(
     <>
